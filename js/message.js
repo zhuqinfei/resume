ff918628b231396eccac1fcf16817e3c676edd04
zhuqinfei <1586859762@qq.com>
2021-09-07 22:16:10 +0800

    在服务器中获取历史留言

--- js/message.js
+++ js/message.js
@@ -4,10 +4,28 @@ AV.init({
     serverURL: "https://s5zf7aoh.lc-cn-n1-shared.com"
   });
 
+  const query = new AV.Query('Message');
+  query.find().then(function(message){
+      console.log(message)
+      console.log(message[0].attributes)
+      let array=message.map((item)=>item.attributes)
+      console.log(array)
+      array.forEach((item)=>{
+         let li=document.createElement('li')
+         li.innerText=`${item.name}:${item.content}`
+         let messageList=document.querySelector('#messageList')
+         messageList.appendChild(li)
+      })
+  }),function(error){
+     alert('提交失败，改天再来')
+  }
+
+
 
 let  myForm=document.querySelector('#postMessageFrom')
 myForm.addEventListener('submit',function(e){
    e.preventDefault()
+   let name=myForm.querySelector('input[name=name]').value
    let content=myForm.querySelector('input[name=content]').value
    //创建 名字为Message的表
    var Message = AV.Object.extend('Message');
@@ -15,9 +33,11 @@ myForm.addEventListener('submit',function(e){
    var message = new Message();
    //数据内容是words:'Hello world!'保存
   //保存成功打印出'保存成功。'
+   message.set('name', name);
    message.set('content', content);
    message.save().then((message)=>{
      console.log("保存成功")
+     window.location.reload()
    })
 })
 
