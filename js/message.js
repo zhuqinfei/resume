8853d1f2dd5f7b7e286f9224818b4549b5a11c83
zhuqinfei <1586859762@qq.com>
2021-09-09 14:53:13 +0800

    完成MVC中M的部分

--- js/message.js
+++ js/message.js
@@ -1,27 +1,45 @@
 !function () {
   var view = document.querySelector('section.message')
 
-  var controller = {
-    view: null,
-    messageList: null,
-    init: function (view) {
-      this.view = view
-      this.initAV()
-      this.messageList = view.querySelector('#messageList')
-      this.form = view.querySelector('#postMessageFrom')
-      this.loadMessages()
-      this.bindEvents()
-    },
+  var model = {
     initAV: function () {
+      //数据库初始化
       AV.init({
         appId: "S5zF7aoH9zRVpy3g10xXpJrC-gzGzoHsz",
         appKey: "a1StNncKamK9yDqGUPqM3KvX",
         serverURL: "https://s5zf7aoh.lc-cn-n1-shared.com"
       });
     },
-    loadMessages:function () {
-      const query = new AV.Query('Message');//文档中用到的代码
-      query.find().then((message) => { //文档中获取对象方法
+    //从数据库中获取数据
+    fetch: function () {
+      const query = new AV.Query('Message');
+      return query.find()    //返回promise对象
+    },
+    //在数据库中创建数据并更新保存数据
+    save: function (name, content) {
+      var Message = AV.Object.extend('Message');
+      var message = new Message();
+      message.set('name', name);
+      message.set('content', content);
+      return message.save()
+    }
+  }
+
+  var controller = {
+    view: null,
+    model: null,
+    messageList: null,
+    init: function (view, model) {
+      this.view = view
+      this.model = model
+      this.model.initAV()
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('#postMessageFrom')
+      this.loadMessages()
+      this.bindEvents()
+    },
+    loadMessages: function () {
+      this.model.fetch().then((message) => {
         let array = message.map((item) => item.attributes)
         array.forEach((item) => {
           let li = document.createElement('li')
@@ -33,35 +51,30 @@
       }
     },
     bindEvents: function () {
-      this.form.addEventListener('submit',  (e)=> {
+      this.form.addEventListener('submit', (e) => {
         e.preventDefault()
-        console.log('here')
         this.saveMessage()
       })
     },
-
-    saveMessage:function(){
+    saveMessage: function () {
       let myForm = this.form
       let name = myForm.querySelector('input[name=name]').value
       let content = myForm.querySelector('input[name=content]').value
-      //创建 名字为Message的表
-      var Message = AV.Object.extend('Message');
-      //在表中创建一行数据
-      var message = new Message();
-      //数据内容是words:'Hello world!'保存
-      //保存成功打印出'保存成功。'
-      message.set('name', name);
-      message.set('content', content);
-      message.save().then((Object) => {
+      console.log(this)
+      this.model.save(name, content).then((Object) => {
         //下面是可以做到无刷新留言
         let li = document.createElement('li')
         li.innerText = `${Object.attributes.name}:${Object.attributes.content}`
         let messageList = document.querySelector('#messageList')
         messageList.appendChild(li)
+        myForm.querySelector('input[name=name]').value = ''
+        myForm.querySelector('input[name=content]').value = ''
       })
     }
   }
-  controller.init(view)
+
+  controller.init(view, model)
+  
 }.call()
 
 
